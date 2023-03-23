3c9dd64e991fc383f528ec0ace3cb65d77f26bd8
Victor da Silva de Oliveira <victor.silva@grupoelv.com.br>
2023-03-23 17:54:56 -0300

    game window

--- main.c
+++ main.c
@@ -4,15 +4,33 @@
 
 int main(void){
 
-    if(SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        printf("Error initializing SLD: %s\n", SDL_GetError());
+    // Initialize video and timer subsystems
+    if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) // If SDL_Init returns 1 show error message
+    {   
+        printf("Error initializing SDL: %s\n", SDL_GetError());
         return 1;
 
     }
 
+    // Create game window.
+    SDL_Window* window = SDL_CreateWindow("MySDL2Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+
+    if(!window){
+        printf("Failed to create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
     printf("SDL Initialized successfully!\n");
 
+    // Wait for 5000ms (5 seconds)
+    SDL_Delay(5000);
+
+    // Remove window
+    SDL_DestroyWindow(window);
+
+    // Release SDL resources from memory to safely quit.
     SDL_Quit();
+    
+    return 1;
 
 }
