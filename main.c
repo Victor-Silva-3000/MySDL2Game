95b428d8059532786cc8eed9bda37434726a2e01
Victor da Silva de Oliveira <victor.silva@grupoelv.com.br>
2023-03-23 16:59:42 -0300

    initialization

--- main.c
+++ main.c
@@ -3,7 +3,16 @@
 
 
 int main(void){
-    printf("Primeiro commit!\n");
-    
-    return 1;
+
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        printf("Error initializing SLD: %s\n", SDL_GetError());
+        return 1;
+
+    }
+
+    printf("SDL Initialized successfully!\n");
+
+    SDL_Quit();
+
 }
